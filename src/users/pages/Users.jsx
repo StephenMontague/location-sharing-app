f7e755c7edfbbf497d659bd53861f9e19228dd57
Stephen Montague <Stephen.D.Montague@gmail.com>
2022-07-23 17:01:24 -0400

    Creation of the UsersList and the UserItem components. Updated the Users page to have a user object and send that array to the UsersList

--- src/users/pages/Users.jsx
+++ src/users/pages/Users.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-	return <h2>Users Works!</h2>;
+	const USERS = [
+		{
+			id: "u1",
+			name: "Stephen",
+			image: "https://media-exp1.licdn.com/dms/image/C4D03AQH1DvxNmEd_MQ/profile-displayphoto-shrink_800_800/0/1628693712151?e=1663804800&v=beta&t=UWgIcHlEI6haOoo0YqLWGwQUwjLFIx_BwQFD5e7GLD0",
+			places: 3,
+		},
+	];
+
+	return <UsersList items={USERS} />;
 };
 
 export default Users;
